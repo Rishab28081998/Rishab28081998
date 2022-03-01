1262d0cf2a37d988d4f61c1de8050b74236add7b
Rishab28081998 <95067159+Rishab28081998@users.noreply.github.com>
2022-03-01 17:37:48 +0530

    Update failure_handler.js

--- failure_handler.js
+++ failure_handler.js
@@ -1,8 +1,24 @@
 response = Map();
-response.put("action","reply");
-response.put("type","Select");
-response.put("replies",{"Hello integration seeker👋 How can we help you today?"});
-// response.put("suggestions",{"I'm looking for support","I'm looking for an integration / Sales Enquiry / Get a Quote","Accounts / Billing Query"}); 
-response.put("input",{"type":"select","options":{"I'm looking for support","I'm looking for an integration / Sales Enquiry / Get a Quote","Accounts / Billing Query"}});
-// info response ;
+if(failed_response.get("action").equalsIgnoreCase("forward"))
+{
+	code = cause.get("code").toNumber();
+	if(code == 1001)
+	{
+		// Outside business hours
+		response.put("action","reply");
+		response.put("replies",{"We're currently unavailable","Leave us a message and we will get back to you shortly."});
+	}
+	else if(code == 1002)
+	{
+		// operators_not_available
+		response.put("action","reply");
+		response.put("replies",{"All our agents are busy at the moment","Please leave us a message and we will get back to you"});
+	}
+	else if(code == 1003)
+	{
+		// invalid_operators - Operators may be disabled or invalid
+		response.put("action","reply");
+		response.put("replies",{"All our agents are busy at the moment","Leave us a message and we will get back to you"});
+	}
+}
 return response;
